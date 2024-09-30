193107a6046c2abf236ca3168a5df3f6040b0ace
Titanoboba <nik20062005@mail.ru>
2024-09-30 13:59:37 +0300

    Started doing Task1.

--- Lab04/Program.cs
+++ Lab04/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class MyMatrix
+{
+    private int[][] matrix;
+    private int intervalmin, intervalmax;
+
+    public int MinInterval
+    {
+        get { return intervalmin; }
+        set { intervalmin = value; }
+    }
+
+    public int MaxInterval
+    {
+        get { return intervalmax; }
+        set { intervalmax = value; }
+    }
+
+    MyMatrix (int m, int n)
+    {
+        this.m = m;
+        this.n = n;
+
+        Random r = new Random();
+        matrix = new int[n][];
+        for (int i = 0; i < n; i++)
+        {
+            matrix[i] = new int[m];
+            for (int j = 0; j < m; j++)
+            {
+                matrix[i][j] = r.Next(intervalmin, intervalmax+1);
+            }
+        }
+    }
+    public static MyMatrix operator *(int num) {}
+
+    public static MyMatrix operator +(MyMatrix m1, MyMatrix m2) {}
+
+    public static MyMatrix operator -(MyMatrix m1, MyMatrix m2) {}
+
+    public static MyMatrix operator *(MyMatrix m1, MyMatrix m2) {}
+
+    public static MyMatrix operator /(int num) {}
+}
